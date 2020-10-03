87384b1e8c5b061790c6127def7dce26ad314ede
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2020-10-03 10:10:09 -0300

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -1,20 +1,17 @@
-var main = document.getElementById('teste');
+var main = document.getElementsByClassName('main__row');
 
 fetch("./js/music.json")
     .then(response => response.json())
     .then((jsonObj) => {
         const songs = jsonObj['songs'];
 
-        for (let i = 0; i < songs.length; i++) {
+        for (let i = 0; i < 10; i++) {
             let myDiv = document.createElement('div');
-
             myDiv = `
             <div class="main__col" data-image="`+ songs[i].dataImage + `" data-artist="` + songs[i].dataArtist + `" data-song="` + songs[i].dataSong + `" data-file="` + songs[i].dataFile + `"><img src="` + songs[i].dataImage + `">
                 <h3>`+ songs[i].songName + `</h3>
                 <p>`+ songs[i].artistName + `</p>
             </div>`;
-
             main.innerHTML = myDiv;
-            console.log(myDiv)
         }
     })
