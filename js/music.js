21f72b088156a06c7ea81c37a28f130bacdd5a12
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2020-10-03 13:33:54 -0300

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -1,25 +1,23 @@
-var main = document.getElementsByClassName('main__row');
+var main = document.getElementById("main__row");
 
 fetch("./js/music.json")
     .then(response => response.json())
     .then((jsonObj) => {
         const albuns = jsonObj['albuns'];
 
-        for (let i = 0; i < 10; i++) {
-            const songs = albuns[i].songs;
-            console.log(songs)
-            let myDiv = document.createElement('div');
-            myDiv.setAttribute("class", "main__col");
-            myDiv.setAttribute("dataImage", `${songs[i].dataImage}`)
-            myDiv.setAttribute("dataArtist", songs[i].dataArtist);
-            myDiv.setAttribute("dataSong", songs[i].dataSong);
-            myDiv.setAttribute("dataFile", songs[i].dataFile);
-            console.log(myDiv);
-
-            let text = document.createTextNode(`<img src="${songs[i].dataImage}">
-           <h3>${songs[i].songName}</h3>
-           <p>${songs[i].artistName}</p>`)
-            myDiv.appendChild(text)
-            document.main.appendChild(myDiv)
+        for (let j = 0; j < albuns.length; j++) {
+            const songs = albuns[j].songs;
+            
+            for (let i = 0; i < songs.length; i++) {
+                let myDiv = document.createElement('div');
+                myDiv.setAttribute("class", "main__col");
+                myDiv.setAttribute("dataImage", `${songs[i].dataImage}`)
+                myDiv.setAttribute("dataArtist", `${songs[i].dataArtist}`);
+                myDiv.setAttribute("dataSong", `${songs[i].dataSong}`);
+                myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
+    
+                myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
+                main.appendChild(myDiv)
+            }
         }
     })
