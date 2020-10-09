aa2b14d089014f41dbc6dbd88cf70ea60196a0e8
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2020-10-08 22:45:00 -0300

    Ajustando álbuns

--- js/music-index.js
+++ js/music-index.js
@@ -1,16 +1,13 @@
-var main__row = document.getElementById("main__row");
+var main__row__title = document.getElementById("main__row__title");
+var main__row__songs = document.getElementById("main__row__songs");
 
 fetch("./js/json/2020-Eng.json")
     .then(response => response.json())
     .then((jsonObj) => {
 
         const albumName = jsonObj['albumName'];
-        let titleDiv = document.createElement('div');
         let title = `<h2>` + albumName + `</h2>`;
-        titleDiv.setAttribute("class", "main__row__title");
-        titleDiv.setAttribute("id", "main__row__title");
-        titleDiv.innerHTML = title;
-        main__row.appendChild(titleDiv);
+        main__row__title.innerHTML = title;
 
         const songs = jsonObj['songs'];
         for (let i = 0; i < songs.length; i++) {
@@ -22,23 +19,20 @@ fetch("./js/json/2020-Eng.json")
             myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
 
             myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
-            main__row.appendChild(myDiv)
+            main__row__songs.appendChild(myDiv)
         }
     })
 
-var main__row = document.getElementById("main__row");
+var main__row__title_1 = document.getElementById("main__row__title_1");
+var main__row__songs_1 = document.getElementById("main__row__songs_1");
 
 fetch("./js/json/2020-Por.json")
     .then(response => response.json())
     .then((jsonObj) => {
 
         const albumName = jsonObj['albumName'];
-        let titleDiv = document.createElement('div');
-        let title = `<h2 style="margin-top: 20px">` + albumName + `</h2>`;
-        titleDiv.setAttribute("class", "main__row__title");
-        titleDiv.setAttribute("id", "main__row__title");
-        titleDiv.innerHTML = title;
-        main__row.appendChild(titleDiv);
+        let title = `<h2>` + albumName + `</h2>`;
+        main__row__title_1.innerHTML = title;
 
         const songs = jsonObj['songs'];
         for (let i = 0; i < songs.length; i++) {
@@ -50,22 +44,20 @@ fetch("./js/json/2020-Por.json")
             myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
 
             myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
-            main__row.appendChild(myDiv)
+            main__row__songs_1.appendChild(myDiv)
         }
     })
-var main__row = document.getElementById("main__row");
 
-fetch("./js/json/2019-Eng.json")
+var main__row__title_2 = document.getElementById("main__row__title_2");
+var main__row__songs_2 = document.getElementById("main__row__songs_2");
+
+fetch("./js/json/2020-Por.json")
     .then(response => response.json())
     .then((jsonObj) => {
 
         const albumName = jsonObj['albumName'];
-        let titleDiv = document.createElement('div');
-        let title = `<h2 style="margin-top: 20px">` + albumName + `</h2>`;
-        titleDiv.setAttribute("class", "main__row__title");
-        titleDiv.setAttribute("id", "main__row__title");
-        titleDiv.innerHTML = title;
-        main__row.appendChild(titleDiv);
+        let title = `<h2>` + albumName + `</h2>`;
+        main__row__title_2.innerHTML = title;
 
         const songs = jsonObj['songs'];
         for (let i = 0; i < songs.length; i++) {
@@ -77,33 +69,31 @@ fetch("./js/json/2019-Eng.json")
             myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
 
             myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
-            main__row.appendChild(myDiv)
+            main__row__songs_2.appendChild(myDiv)
         }
     })
-    var main__row = document.getElementById("main__row");
 
-    fetch("./js/json/2019-Por.json")
-        .then(response => response.json())
-        .then((jsonObj) => {
-    
-            const albumName = jsonObj['albumName'];
-            let titleDiv = document.createElement('div');
-            let title = `<h2 style="margin-top: 20px">` + albumName + `</h2>`;
-            titleDiv.setAttribute("class", "main__row__title");
-            titleDiv.setAttribute("id", "main__row__title");
-            titleDiv.innerHTML = title;
-            main__row.appendChild(titleDiv);
-    
-            const songs = jsonObj['songs'];
-            for (let i = 0; i < songs.length; i++) {
-                let myDiv = document.createElement('div');
-                myDiv.setAttribute("class", "main__col");
-                myDiv.setAttribute("dataImage", `${songs[i].dataImage}`)
-                myDiv.setAttribute("dataArtist", `${songs[i].dataArtist}`);
-                myDiv.setAttribute("dataSong", `${songs[i].dataSong}`);
-                myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
-    
-                myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
-                main__row.appendChild(myDiv)
-            }
-        })
+var main__row__title_3 = document.getElementById("main__row__title_3");
+var main__row__songs_3 = document.getElementById("main__row__songs_3");
+
+fetch("./js/json/2020-Por.json")
+    .then(response => response.json())
+    .then((jsonObj) => {
+
+        const albumName = jsonObj['albumName'];
+        let title = `<h2>` + albumName + `</h2>`;
+        main__row__title_3.innerHTML = title;
+
+        const songs = jsonObj['songs'];
+        for (let i = 0; i < songs.length; i++) {
+            let myDiv = document.createElement('div');
+            myDiv.setAttribute("class", "main__col");
+            myDiv.setAttribute("dataImage", `${songs[i].dataImage}`)
+            myDiv.setAttribute("dataArtist", `${songs[i].dataArtist}`);
+            myDiv.setAttribute("dataSong", `${songs[i].dataSong}`);
+            myDiv.setAttribute("dataFile", `${songs[i].dataFile}`);
+
+            myDiv.innerHTML = `<img src="${songs[i].dataImage}" /><h3>${songs[i].songName}</h3><p>${songs[i].artistName}</p>`;
+            main__row__songs_3.appendChild(myDiv)
+        }
+    })
