36275efeb708e76b6372d09b2789b8fb3e80be2b
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2020-10-28 15:51:18 -0300

    Pegando músicas dinamicamente

--- js/music-playlist.js
+++ js/music-playlist.js
@@ -5,17 +5,17 @@ fetch("./js/json/albuns.json")
     .then((jsonObj) => {
 
         const albuns = jsonObj['albuns'];
-        console.log(albuns)
         for (let i = 0; i < albuns.length; i++) {
-            let myTr = document.createElement('tr');
-            let myFirstTd = document.createElement('td');
-            let mySecondTd = document.createElement('td');
-            let myThirdTd = document.createElement('td');
-            let myImg = document.createElement('img');
-            let myFirstP = document.createElement('p');
-            let mySecondP = document.createElement('p');
+            let j = 0;
+            albuns[i].songs.forEach(() => {
+                let myTr = document.createElement('tr');
+                let myFirstTd = document.createElement('td');
+                let mySecondTd = document.createElement('td');
+                let myThirdTd = document.createElement('td');
+                let myImg = document.createElement('img');
+                let myFirstP = document.createElement('p');
+                let mySecondP = document.createElement('p');
 
-            for (let j = 0; j < albuns[i].songs.length; j++) {
                 myImg.setAttribute("src", albuns[i].songs[j].dataImage);
                 myFirstP.innerHTML = albuns[i].songs[j].songName;
                 mySecondP.innerHTML = albuns[i].songs[j].artistName;
@@ -26,6 +26,7 @@ fetch("./js/json/albuns.json")
                 myTr.appendChild(mySecondTd);
                 myTr.appendChild(myThirdTd);
                 table__body.appendChild(myTr);
-            }
+                j++;
+            });
         }
     })
