13e20a86cce280eeff0c0ca6897146a9173d39f7
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2020-12-23 15:56:05 -0300

    Service Worker Atualizado

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,23 @@
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('sw.js')
+//     .then(function (reg) {
+//       console.log('Registration succeeded. Scope is ' + reg.scope);
+//     }).catch(function (error) {
+//       console.log('Registration failed with ' + error);
+//     });
+// }
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', {scope: '/public/'})
-  .then(function(reg) {
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    console.log('Registration failed with ' + error);
-  });
+  navigator.serviceWorker.register('sw.js')
+    .then(function (reg) {
+      if (reg.installing) {
+        console.log('Service worker installing ' + reg.scope);
+      } else if (reg.waiting) {
+        console.log('Service worker installed ' + reg.scope);
+      } else if (reg.active) {
+        console.log('Service worker active ' + reg.scope);
+      }
+    }).catch(function (error) {
+      console.log('Registration failed with ' + error);
+    });
 }
--- public/sw.js
+++ public/sw.js
@@ -1,72 +1,70 @@
-var actualDate = new Date();
-setTimeout(() => {
-    var cacheName = `cache-lds-${actualDate}`;
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/pages/about.html',
-                '/pages/library.html',
-                '/pages/playlist.html',
-                '/pages/offline.html',
-                '/assets/logo.png',
-                '/assets/musicas_curtidas.png',
-                '/assets/favicon-16x16.png',
-                '/assets/favicon-32x32.png',
-                '/assets/android-chrome-192x192.png',
-                '/assets/android-chrome-512x512.png',
-                '/assets/apple-touch-icon.png',
-                '/styles/main.css',
-                '/styles/main-about.css',
-                '/styles/main-library.css',
-                '/styles/main-playlist.css',
-                '/styles/player.css',
-                '/styles/player-playlist.css',
-                '/styles/reset.css',
-                '/styles/responsive.css',
-                '/styles/responsive-about.css',
-                '/styles/responsive-library.css',
-                '/styles/responsive-playlist.css',
-                '/styles/sidebar.css',
-                '/scripts/index.js',
-                '/scripts/library.js',
-                '/scripts/music-index.js',
-                '/scripts/music-playlist.js',
-                '/scripts/player-playlist.js',
-                '/scripts/player.js',
-                '/scripts/json/albuns.json',
+                '/public/',
+                '/public/index.html',
+                '/public/pages/about.html',
+                '/public/pages/library.html',
+                '/public/pages/playlist.html',
+                '/public/pages/offline.html',
+                '/public/assets/logo.png',
+                '/public/assets/musicas_curtidas.png',
+                '/public/assets/favicon-16x16.png',
+                '/public/assets/favicon-32x32.png',
+                '/public/assets/android-chrome-192x192.png',
+                '/public/assets/android-chrome-512x512.png',
+                '/public/assets/apple-touch-icon.png',
+                '/public/styles/main.css',
+                '/public/styles/main-about.css',
+                '/public/styles/main-library.css',
+                '/public/styles/main-playlist.css',
+                '/public/styles/player.css',
+                '/public/styles/player-playlist.css',
+                '/public/styles/reset.css',
+                '/public/styles/responsive.css',
+                '/public/styles/responsive-about.css',
+                '/public/styles/responsive-library.css',
+                '/public/styles/responsive-playlist.css',
+                '/public/styles/sidebar.css',
+                '/public/scripts/index.js',
+                '/public/scripts/library.js',
+                '/public/scripts/music-index.js',
+                '/public/scripts/music-playlist.js',
+                '/public/scripts/player-playlist.js',
+                '/public/scripts/player.js',
+                '/public/scripts/json/albuns.json',
             ]);
         })
     );
 });
+self.addEventListener('activate', (event) => {
+    var cacheKeeplist = ['v2'];
 
-// Clear cache on activate
-this.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cacheName => (cacheName.startsWith('cache-lds-')))
-                    .filter(cacheName => (cacheName !== cacheName))
-                    .map(cacheName => caches.delete(cacheName))
-            );
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
         })
     );
 });
-// Serve from Cache
-this.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-            console.log(response)
-          return response || fetch(event.request);
-        })
-        .catch(() => {
-            console.log('offline: '+response)
-          return caches.match('pages/offline.html');
-        })
-    )
-  });
-}, 5000);
+        caches.match(event.request)
+            .then((resp) => {
+                return resp || fetch(event.request).then((response) => {
+                    let responseClone = response.clone();
+                    caches.open('v1').then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                    return response;
+                }).catch(() => {
+                    return caches.match('./public/pages/offline.html');
+                })
+            })
+    );
+});
