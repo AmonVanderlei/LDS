c12aed4b9b7a04191c2baaddcd4276fad7f97ed9
Amon Vanderlei <65919312+AmonVanderlei@users.noreply.github.com>
2021-01-13 21:50:55 -0300

    filtros adicionados

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -27,6 +27,86 @@ fetch("../scripts/json/albuns.json")
         }
     })
 
+let filter1 = document.querySelector('#filter1');
+let filter1label = document.querySelector('.filter1label');
+let filter1Children = document.querySelectorAll('.filter1-children');
+let filter2 = document.querySelector('#filter2');
+let filter2label = document.querySelector('.filter2label');
+let filter2Children = document.querySelectorAll('.filter2-children');
+
+filter1label.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (filter1.checked == true) {
+        filter1Children[0].classList.remove('green');
+        filter1Children[1].classList.remove('green');
+        filter1.checked = false;
+    } else {
+        filter1Children[0].classList.add('green');
+        filter1Children[1].classList.add('green');
+        filter1.checked = true;
+        if (filter2.checked == true) {
+            filter2Children[0].classList.remove('green');
+            filter2Children[1].classList.remove('green');
+            filter2.checked = false;
+        }
+    }
+    if (filter1.checked == true) {
+        sortAZElements()
+    } else {
+        if (document.querySelector(".filter").value != "") {
+            let wrote = document.querySelector(".filter").value.toLowerCase();
+            let = filtered = [];
+            elements.forEach(element => {
+                if (element.attributes.datasong.value.toLowerCase().indexOf(wrote) != -1) {
+                    filtered.push(element)
+                }
+            });
+            createMainCol(filtered)
+        } else {
+            createMainCol(elements)
+        }
+    }
+    return false;
+});
+
+filter2label.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (filter2.checked == true) {
+        filter2Children[0].classList.remove('green');
+        filter2Children[1].classList.remove('green');
+        filter2.checked = false;
+    } else {
+        filter2Children[0].classList.add('green');
+        filter2Children[1].classList.add('green');
+        filter2.checked = true;
+        if (filter1.checked == true) {
+            filter1Children[0].classList.remove('green');
+            filter1Children[1].classList.remove('green');
+            filter1.checked = false;
+        }
+    }
+
+    if (filter2.checked == true) {
+        sortZAElements()
+    } else {
+        if (document.querySelector(".filter").value != "") {
+            let wrote = document.querySelector(".filter").value.toLowerCase();
+            let = filtered = [];
+            elements.forEach(element => {
+                if (element.attributes.datasong.value.toLowerCase().indexOf(wrote) != -1) {
+                    filtered.push(element)
+                }
+            });
+            createMainCol(filtered)
+        } else {
+            createMainCol(elements)
+        }
+    }
+    return false;
+});
+
 const debounceEvent = (fn, wait = 1000, time) => (...args) => {
     clearTimeout(time, time = setTimeout(() => fn(...args), wait))
 }
@@ -40,6 +120,12 @@ function handleKeyUp(event) {
         }
     });
     createMainCol(filtered)
+    if (filter1.checked == true) {
+        sortAZElements()
+    }
+    if (filter2.checked == true) {
+        sortZAElements()
+    }
 }
 
 function createMainCol(filteredElements) {
@@ -53,3 +139,46 @@ function createMainCol(filteredElements) {
 }
 
 document.querySelector(".filter").addEventListener("keyup", debounceEvent(handleKeyUp, 500))
+
+function sortAZElements() {
+    let elementsNodeList = document.querySelectorAll('.main__col');
+    let elementsToSort = Array.from(elementsNodeList);
+
+    let elementsName = [];
+    elementsToSort.forEach(element => {
+        elementsName.push(element.attributes.datasong.value);
+    });
+    let sortedElementsName = elementsName.sort();
+
+    let sortedElements = []
+    sortedElementsName.forEach(sortedElementName => {
+        elementsToSort.forEach(element => {
+            if (sortedElementName == element.attributes.datasong.value) {
+                sortedElements.push(element);
+            }
+        })
+    })
+    createMainCol(sortedElements)
+}
+
+function sortZAElements() {
+    let elementsNodeList = document.querySelectorAll('.main__col');
+    let elementsToSort = Array.from(elementsNodeList);
+
+    let elementsName = [];
+    elementsToSort.forEach(element => {
+        elementsName.push(element.attributes.datasong.value);
+    });
+    let sortedElementsName = elementsName.sort();
+
+    let sortedElements = []
+    sortedElementsName.forEach(sortedElementName => {
+        elementsToSort.forEach(element => {
+            if (sortedElementName == element.attributes.datasong.value) {
+                sortedElements.push(element);
+            }
+        })
+    })
+    let sortedZAElements = sortedElements.slice(0).reverse();
+    createMainCol(sortedZAElements)
+}
